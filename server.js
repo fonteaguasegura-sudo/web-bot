6fa3e8d138f1ea213cfb9a458606a7b7d864cc8d
fonteaguasegura <fonteaguasegura@example.com>
2026-01-26 11:06:42 +0000

    🚀 Perf: Skip puppeteer download and use pre-installed Chrome

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ function initializeWhatsAppClient() {
         }),
         puppeteer: {
             headless: true,
-            executablePath: puppeteer.executablePath(),
+            executablePath: process.env.PUPPETEER_SKIP_DOWNLOAD === 'true' ? '/usr/bin/google-chrome' : puppeteer.executablePath(),
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
