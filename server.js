39dce9bb57bd5c37e7b531321abb5fe1febe70d6
fonteaguasegura <fonteaguasegura@example.com>
2026-01-26 14:23:30 +0000

    ✨ fix: Resolve Render deployment issue by using puppeteer-core and pre-installed Chromium

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const qrcode = require('qrcode');
 const fs = require('fs');
 const db = require('./database.js');
 const puppeteer = require('puppeteer-core');
-const chrome = require('@sparticuz/chrome-aws-lambda');
+const chromium = require('@sparticuz/chromium');
 
 const app = express();
 const server = http.createServer(app);
@@ -31,7 +31,7 @@ let clientReady = false;
 const dataPath = path.join(__dirname, '.wwebjs_auth');
 
 async function initializeWhatsAppClient() {
-    const executablePath = await chrome.executablePath;
+    const executablePath = await chromium.executablePath();
 
     client = new Client({
         authStrategy: new LocalAuth({
@@ -39,9 +39,9 @@ async function initializeWhatsAppClient() {
             dataPath: "./.wwebjs_auth"
         }),
         puppeteer: {
-            headless: chrome.headless,
+            headless: chromium.headless,
             executablePath,
-            args: chrome.args
+            args: chromium.args
         },
         webVersionCache: {
             type: 'remote',
